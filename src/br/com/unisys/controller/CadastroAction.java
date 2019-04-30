11034a0046f6c434fb44b6ed05bc27be84abcfdc
dsfdarlan-32 <dsfdarlan-32@hotmail.com>
2019-04-29 21:46:41 -0300

    ultima Versão

--- src/br/com/unisys/controller/CadastroAction.java
+++ src/br/com/unisys/controller/CadastroAction.java
@@ -1,12 +1,13 @@
 package br.com.unisys.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class CadastroAction extends HttpServlet {
+public class CadastroAction extends HttpServlet{
 	
 	private static final long serialVersionUID = -6359616861621683724L;
 
