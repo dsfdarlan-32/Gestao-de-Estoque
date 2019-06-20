1d55dcb53e4aa46a56197b342f7129c8fa23a4cf
dsfdarlan-32 <dsfdarlan-32@hotmail.com>
2019-06-19 23:23:39 -0300

    subir

--- src/connection/BancoDeDados.java
+++ src/connection/BancoDeDados.java
@@ -9,32 +9,24 @@ public class BancoDeDados {
   
   public Connection getConnection() {
 	  Connection cn = null;
-	  
 	  try {
-		  
 		  Class.forName("com.mysql.jdbc.Driver");
-
 		  String url = "jdbc:mysql://localhost/ESTOQUE";
 		  String username = "darlan"; 
 		  String password = "Dddoline99977244@";
-		  
 		  cn = DriverManager.getConnection(url, username,password);
-	
 		} catch (ClassNotFoundException e) {
 			System.out.println(e.getMessage());
 			e.printStackTrace();
-			
 		} catch (SQLException e) {
 			System.out.println(e.getMessage());
 			e.printStackTrace();
 		}
-	 
 	  return cn;
  }
   
   public static void main(String[] args) {
 	  BancoDeDados bancoDeDados = new BancoDeDados();
 	  System.out.println(bancoDeDados.getConnection());
-
 	}
 }
